b33ce1ac389f5cd55af50c6519ccd171703b63dd
yad <yad.hoshyar@gmail.com>
2026-02-12 00:05:28 +0300

    Add authentication features with Laravel Sanctum and create personal access tokens table

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::prefix('auth')->name('auth.')->group(function () {
+//    Route::post('token', [AuthController::class, 'createToken'])->name('create-token');
+//    Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
+//    Route::post('verify-reset-code', [AuthController::class, 'verifyResetCode'])->name('verify-reset-code');
+//    Route::post('reset-password-with-token', [AuthController::class, 'resetPasswordWithToken'])->name('reset-password-with-token');
+//});
