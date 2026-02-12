4769cd2a10e8b5936b147fce0135fee6c56d8de5
yad <yad.hoshyar@gmail.com>
2026-02-13 01:22:01 +0300

    Add Scramble configuration and enforce testing rules in documentation

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,17 +10,19 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
+
     protected function casts(): array
     {
         return [
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,13 @@
 
 use App\Http\Controllers\Api\NotificationController;
 use App\Http\Controllers\Auth\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::prefix('auth')->name('auth.')->group(function () {
+// Route::prefix('auth')->name('auth.')->group(function () {
 //    Route::post('token', [AuthController::class, 'createToken'])->name('create-token');
 //    Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
 //    Route::post('verify-reset-code', [AuthController::class, 'verifyResetCode'])->name('verify-reset-code');
 //    Route::post('reset-password-with-token', [AuthController::class, 'resetPasswordWithToken'])->name('reset-password-with-token');
-//});
+// });
 
 Route::get('notifications', [NotificationController::class, 'index'])->name('notifications.index');
